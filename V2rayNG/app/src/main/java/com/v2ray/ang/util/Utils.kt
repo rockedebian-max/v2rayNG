c9cb35070652a151bc98ea68f9ae18258d158538
ELHACKERR <patitojuan34000@gmail.com>
2026-02-22 11:21:02 -0600

    v2.7.1 BETA

--- V2rayNG/app/src/main/java/com/v2ray/ang/util/Utils.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/util/Utils.kt
@@ -533,7 +533,7 @@ object Utils {
      *
      * @return True if the package is Xray, false otherwise.
      */
-    fun isXray(): Boolean = BuildConfig.APPLICATION_ID.startsWith("com.v2ray.ang")
+    fun isXray(): Boolean = BuildConfig.APPLICATION_ID.startsWith("com.cyberguard.vpn")
 
     /**
      * Check if it is the Google Play version.
